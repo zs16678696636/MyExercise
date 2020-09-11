511373c19c53ab099ece023b244647cb0aa6e757
zs <zsacc166@163.com>
2020-09-11 15:49:51 +0800

    0911 提交

--- src/views/lx/ccLx.vue
+++ src/views/lx/ccLx.vue
@@ -1,6 +1,12 @@
 <template>
   <div class='ccLx'>
-
+    <button @click="lx">dianwo</button>
+    <div class="father">
+      <div class="son"></div>
+      <div class="son"></div>
+      <div class="son3"></div>
+      <!-- <div class="son4"></div> -->
+    </div>
   </div>
 </template>
 
@@ -9,7 +15,15 @@ export default {
   data () {
     return {}
   },
-  methods: {},
+  methods: {
+    lx () {
+      let aa = new Image()
+      aa.src = '../../assets/img/kscj.png'
+      let son = document.querySelector('.son')
+      son.innerHTML = aa
+      console.dir(aa)
+    }
+  },
   filters: {},
   computed: {},
   components: {},
@@ -20,16 +34,39 @@ export default {
 
 <style lang="less" scoped>
 .ccLx {
-  width: 6rem;
-  .ym {
-    width: 200px;
-    height: 200px;
-    background-color: red;
-  }
-  .ym2 {
-    width: 200px;
-    height: 200px;
-    background-color: green;
+  .father {
+    margin-left: 10px;
+    width: 600px;
+    height: 500px;
+    background-color: rgba(122, 233, 145);
+    display: grid;
+    // place-items: center; // 水平垂直居中
+    // grid-template-columns: 250px repeat(2, 1fr) 50px;
+    // grid-column-gap: 10px;
+    display: grid;
+    // flex-direction: column;
+    grid-template-rows: auto 1fr auto;
+    .son {
+      height: 100px;
+      width: 100%;
+      background-color: rgba(191, 246, 214);
+    }
+    .son2 {
+      // flex: 1 0 auto;
+      // height: 100px;
+      width: 100%;
+      background-color: rgb(12, 29, 19);
+    }
+    .son3 {
+      height: 100px;
+      width: 100%;
+      background-color: rgb(35, 122, 71);
+    }
+    .son4 {
+      height: 100px;
+      width: 100%;
+      background-color: rgb(34, 243, 121);
+    }
   }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,48 @@
-const path = require('path')
-console.log(1111111, process.argv[3])
-console.log(222, process.env.NODE_ENV)
-module.exports = {
-  devServer: {
-    port: 8085, // 端口号
-    open: true, // 自动打开浏览器
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8888/api/private/v1/',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' }
-      }
-    } // /api开头自动拼接并把/api转成空
-  },
-  configureWebpack: {}
-}
+// const path = require('path')
+// console.log(1111111, process.argv[3])
+// console.log(222, process.env.NODE_ENV)
+// const CompressionWebpackPlugin = require('compression-webpack-plugin') // 压缩插件
+// // 如果此时将项目部署到已开启了gzip的服务器如nginx里面之后，访问浏览器即可看到浏览器下载的是已压缩的文件
+// const isProd = process.env.NODE_ENV === 'production' // 判断是否是生产
+// const webpack = require('webpack')
+// import moment from 'moment'
+// //手动引入所需要的语言包
+// import 'moment/locale/zh-cn';
+// // 指定使用的语言
+// moment.locale('zh-cn');
+// // ====
+
+// module.exports = {
+//   devServer: {
+//     port: 8085, // 端口号
+//     open: true, // 自动打开浏览器
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8888/api/private/v1/',
+//         changeOrigin: true,
+//         pathRewrite: { '^/api': '' }
+//       }
+//     } // /api开头自动拼接并把/api转成空
+//   },
+//   configureWebpack: config => {
+//     // 判断是否是生产
+//     if (isProd) {
+//       // 配置webpack 压缩
+//       config.plugins.push(
+//         new CompressionWebpackPlugin({
+//           test: /\.js$|\.html$|\.css$/,
+//           // 超过4kb压缩
+//           threshold: 4096
+//         })
+//       )
+//     }
+//   },
+//   // configureWebpack: {}
+//   chainWebpack: config => {
+//     // 优化moment 去掉国际化内容
+//     config
+//     .plugin('ignore')
+//     // 忽略/moment/locale下的所有文件
+//     .use(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)) 
+//   }
+// }
